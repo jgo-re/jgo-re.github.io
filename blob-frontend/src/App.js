66aba9cb82047c0805215b68b1565debd5a92a9c
jgo-re <contact@jakegore.co.uk>
2022-04-18 08:58:12 +0100

    Lock the UI when creating a blob, prevents issues when the server is cold

--- blob-frontend/src/App.js
+++ blob-frontend/src/App.js
@@ -1,82 +1,101 @@
-import { BrowserRouter as Router, Routes, Route, useSearchParams } from "react-router-dom";
-import GetBlob from './GetBlob'
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useSearchParams,
+} from "react-router-dom";
+import GetBlob from "./GetBlob";
+import "./App.css";
 
-const API_URL = 'https://blob-api-go.herokuapp.com/b/'
+const API_URL = "https://blob-api-go.herokuapp.com/b/";
 
 export default () => {
   return (
     <Router>
       <h1 className="nice-font">Blob.</h1>
       <Routes>
-        <Route path="/" element={<Blob/>}/>
+        <Route path="/" element={<Blob />} />
       </Routes>
     </Router>
   );
-}
+};
 
-function Blob(){
+function Blob() {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  if (searchParams.has('b')) {
-    return ViewBlob(searchParams.get('b'));
+  if (searchParams.has("b")) {
+    return ViewBlob(searchParams.get("b"));
   }
 
   return NewBlob();
 }
 
-function NewBlob(){
+function NewBlob() {
   console.log("New Blob");
   return (
-      <>
-      <p>Save a blob of text for 15 mins by entering it in the box below and pressing 'Save Blob'.</p>
-      <textarea/>
+    <>
+      <p>
+        Save a blob of text for 15 mins by entering it in the box below and
+        pressing 'Save Blob'.
+      </p>
+      <textarea />
       <button onClick={CallCreateBlob}>Save Blob</button>
-      </>
+    </>
   );
 }
 
-function CallCreateBlob(){
-  let val = document.querySelector('textarea').value;
-  if(val == '')
-    return;
+function CallCreateBlob(e) {
+  const btn = e.currentTarget;
+  const textarea = document.querySelector("textarea");
+  let val = textarea.value;
+  if (val == "") return;
+
+  textarea.disabled = true;
+  btn.disabled = true;
 
   let json = `{"Value":"${encodeURI(val)}"}`;
-  fetch(API_URL, { method: 'POST', body: json})
-  .then(response => response.json())
-  .then(response => {
-    console.log(response);
-    window.location = `/?b=${response}`;
-  });
+
+  fetch(API_URL, { method: "POST", body: json })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      window.location = `/?b=${response}`;
+    })
+    .catch((error) => {
+      btn.disabled = false;
+      textarea.disabled = false;
+      alert("Something went wrong! Please try again later.");
+    });
 }
 
-function ViewBlob(key){
+function ViewBlob(key) {
   console.log("View Blob");
 
-  var {value, loading, error} = GetBlob(key, API_URL);
+  var { value, loading, error } = GetBlob(key, API_URL);
   console.log(value);
 
-  if (loading){
-    return <textarea value={value} readOnly={true}/>
-  } 
+  if (loading) {
+    return <textarea value={value} readOnly={true} />;
+  }
 
   if (error) {
     window.location = `/`;
-  } 
+  }
 
   return (
     <>
-    <textarea value={value} readOnly={false}/>
-    <div className="wrapper">
-      <button onClick={CopyBlob}>Copy Blob</button><button onClick={MakeNewBlob}>Make New Blob</button><button onClick={CopyLink}>Copy Blob Link</button>
-    </div>
+      <textarea value={value} readOnly={false} />
+      <div className="wrapper">
+        <button onClick={CopyBlob}>Copy Blob</button>
+        <button onClick={MakeNewBlob}>Make New Blob</button>
+        <button onClick={CopyLink}>Copy Blob Link</button>
+      </div>
     </>
-    
   );
 }
 
 function CopyBlob() {
-  let val = document.querySelector('textarea').value;
+  let val = document.querySelector("textarea").value;
   navigator.clipboard.writeText(val);
 }
 
